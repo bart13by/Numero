392b6e89c341a705ec97214b36ba3e3a7a539ab5
Patrick Bryant <patrick.bryant@emory.edu>
2023-05-11 15:16:43 -0400

    Fixed the bug I created fixing the last bug, plus clean up the code a bit.

--- JS/numero.js
+++ JS/numero.js
@@ -1,12 +1,12 @@
 const GUESSES_ALLOWED = 6;
 const NUM_DIGITS = 4;
-let BLOCK_INPUT = false;
 let LETTER_NODES = [];
 let GUESSES = [];
 let IN_PROGRESS = [];
 let SOLUTION;
 
 function Guess(index, digit, correct=false, in_answer=false){
+	// "class" for holding guess objects
 	return {
 		'index': index,
 		'digit': digit,
@@ -15,34 +15,20 @@ function Guess(index, digit, correct=false, in_answer=false){
 	}
 
 }
-function handleReset(event){
-	console.log("in reset");
-	document.getElementById("grid").style.display = "grid";
-	document.getElementById('reset').style.display = 'none';
-	
-	BLOCK_INPUT = false;
-	LETTER_NODES = [];
-	GUESSES = [];
-	IN_PROGRESS = [];
-	main();
-	redraw();
-	for (const child of document.getElementById('letters').children){
-		child.innerHTML = '';
-		child.classList.remove(...child.classList);
-		child.classList.add("letter");
-
-	}
-
-}
 function main(){
+	/* initial set up function. Called on load */
 
+
+	// Create an array of random NUM_DIGITS integers 0-9
 	let s = [];
 	for (let i = 0; i < 4; i++){
 		const randomInt = Math.floor(Math.random() * 10);
 		s.push(randomInt);
 	}
-	SOLUTION = s.join('');
-	console.log(`solution is ${SOLUTION}`);
+	SOLUTION = s.join(''); // join the array into a 4-digit number string
+	console.log(`solution is ${SOLUTION}`); // output for debugging/cheating
+
+	// add handlers to all the number elements
 	const numberInputs = document.getElementById('grid').children;
 	for (const child of numberInputs){
 		child.addEventListener('click', handleNumberClick);
@@ -52,10 +38,7 @@ function main(){
 	document.getElementById('reset').addEventListener('click', handleReset);
 	document.addEventListener('keydown', handleKeyPress);
 	
-	
-	
-
-	//load up data structure
+	//Create an array of arrays of Guess objects to update as user makes guesses 
 	for (let i = 0; i < GUESSES_ALLOWED; i++){
 		let ltr_row = [];
 		for (let n = 0; n < NUM_DIGITS; n++){
@@ -64,9 +47,34 @@ function main(){
 		}
 		LETTER_NODES.push(ltr_row);
 	}
+	redraw();
+}
+
+
+function handleReset(event){
+	/* Event handler to clean up and start over */
+	
+	// Reset CSS stuff
+	document.getElementById("grid").style.display = "grid";
+	document.getElementById('reset').style.display = 'none';
+	for (const child of document.getElementById('letters').children){
+		child.innerHTML = '';
+		child.classList.remove(...child.classList);
+		child.classList.add("letter");
+
+	}
+	// Empty out globals
+	LETTER_NODES = [];
+	GUESSES = [];
+	IN_PROGRESS = [];
+    // Run main again
+	main();
+	
 }
 
 function handleKeyPress(event){
+	/* Allow user to use the keypad to enter guesses*/
+
 	// we need to let Enter and Backspace through before the block
 	if (event.key === 'Enter') {
 		handleEnter();
@@ -76,44 +84,50 @@ function handleKeyPress(event){
 		handleDelete();
 		return;
 	}
-	if (BLOCK_INPUT) return;
+	// Exit if the in_progress row is full
 	if (IN_PROGRESS.length >= NUM_DIGITS) return;
+	// Exit if the key is not a number
 	if (!'0123456789'.includes(event.key)) return;
-	const num = event.key;
-	
-	IN_PROGRESS.push(num);
+	// add the number to the row and redraw
+	IN_PROGRESS.push(event.key);
 	redraw();
 }
+
 function handleNumberClick(event){
-	const num = event.target.innerHTML;
-	if (BLOCK_INPUT) return;
+	/* Handle click/tap on the on-screen number pad */
+
+	// Exit if the input row is full
 	if (IN_PROGRESS.length >= NUM_DIGITS) return;
 	
-	IN_PROGRESS.push(num);
+	// Add the number to the row and redraw
+	IN_PROGRESS.push(event.target.innerHTML);
 	redraw();
 }
 function redraw(){
-	// iterate through guesses then current
-	const next_row = GUESSES.length;
-	for (const row_num in GUESSES){
+	/* 
+	 * Redraw the UI. LETTER_NODES is an array of elements for displaying the guesses;
+     * GUESSES is an array of rows of guess objects
+	*/
+
+	// First redraw all the guesses already in the bank
+	const next_row = GUESSES.length; // get the index for the next row
+	for (const row_num in GUESSES){ //iterate through the rows o
 		const row = GUESSES[row_num];
-		for (const node_num in row){
+		for (const node_num in row){ // iterate through the digits in each guess
 			const guess_node = row[node_num];
 			const element = LETTER_NODES[row_num][node_num];
-			element.innerHTML = guess_node.digit; // format 
-			if (guess_node.correct){
+			element.innerHTML = guess_node.digit; // set the value of the digit
+			if (guess_node.correct){ // set CSS to indicate status
 				element.classList.add("correct");
-				//element.style.backgroundColor = "green";	
 			}else if (guess_node.in_answer) {
 				element.classList.add("in-answer");
-				//element.style.backgroundColor = "yellow";
 			}else{
 				element.classList.add("incorrect");
-				//element.style.backgroundColor = "lightgray";
 			}
 			
 		}
 	}
+	// Now redraw any guesses in progress
 	for (let i = 0; i < NUM_DIGITS; i++){
 		LETTER_NODES[next_row][i].innerHTML = '';
 	}
@@ -123,10 +137,12 @@ function redraw(){
 }
 
 function handleEnter(){
-	if (IN_PROGRESS.length < 4){
-		return;
-	}
-	// First I'm going to make an array of Guess objects
+	/* Key or button, get the IN_PROGRESS guess digits and put them in the bank */
+
+	// Exit if the row is not full
+	if (IN_PROGRESS.length < NUM_DIGITS) return;
+	
+	
 	let guesses = [];
 	for (const position in IN_PROGRESS){	
 		guesses.push(Guess(position, IN_PROGRESS[position]));
@@ -158,23 +174,24 @@ function handleEnter(){
 
 	IN_PROGRESS = [];
 	redraw();
-	BLOCK_INPUT = false;
+	
 
 	function solved(solved){
-		BLOCK_INPUT = true;
+		/* Close the puzzle for input and do the final "solve state" formatting */
+		document.removeEventListener('keydown', handleKeyPress);
 		document.getElementById("grid").style.display = "none";
-		if (solved){
+		if (solved){ // Ugh, I should change these to classes
 			document.getElementById("word").style.backgroundColor = "green";
 		}else{
 			document.getElementById("word").style.backgroundColor = "lightgray";
 		}
 		document.getElementById("word").innerHTML = `${SOLUTION}`;
 		document.getElementById('reset').style.display = 'block';
-	
 	}
 }
 
 function handleDelete(){
+	/* Self-explanatory */
 	IN_PROGRESS.pop();
 	redraw();
 }
